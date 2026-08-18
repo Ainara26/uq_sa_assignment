1f0264117491e769bc83acd77bddc0be79aee16a
Ainara26 <s233639@dtu.dk>
2026-08-18 09:40:08 +0200

    change parameters to be the input of the function

--- src/uq_sa_assignment/borehole_func.py
+++ src/uq_sa_assignment/borehole_func.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def func(x, args):
-    t_u, h_u, h_l, r, r_w, l, t_l, k_w = args
-
+def func(x):
+    x = x.T
+    r_w, r, t_u, h_u, t_l, h_l, l, k_w = x
     num = 2*np.pi*t_u*(h_u-h_l)
-    den = np.log(r/r_w)*(1+(2*l*t_u/np.log(r/r_w)*r_w**2*k_w)+(t_u/t_l))
+    den = np.log(r/r_w)*(1+((2*l*t_u)/(np.log(r/r_w)*r_w**2*k_w))+(t_u/t_l))
 
     return num/den
